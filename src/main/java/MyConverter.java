77ea6df3c4043b46de18b4e3323d72b070008fa0
Sunil Poudel <sunil.poudel@citytech.global>
2019-02-07 13:06:15 +0545

    formatted

--- src/main/java/MyConverter.java
+++ src/main/java/MyConverter.java
@@ -2,36 +2,46 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class MyConverter {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         final String SOURCE_PDF_DIR = "/home/sunil/Desktop/pdftoimage/sample.pdf";
         final String DEST_DIR = "/home/sunil/Desktop/pdftoimage/converted/";
         final String OUT_IMAGE_FORMAT = "jpg";
 
-        byte[] data = Files.readAllBytes(Paths.get(SOURCE_PDF_DIR));
+        try {
+            byte[] data = Files.readAllBytes(Paths.get(SOURCE_PDF_DIR));
 
-        List<byte[]> convertedImages = new PDF2ImageConverter.Builder(data)
-                .build()
-                .convertToImage();
+            List<byte[]> convertedImages = new PDF2ImageConverter.Builder(data)
+                    .outputImageFormat(OUT_IMAGE_FORMAT)
+                    .build()
+                    .convertToImage();
 
 
-        System.out.println("converted total images : " + convertedImages);
-        int imageNumber = 0;
-        for (byte[] cImage : convertedImages) {
-            InputStream inputStream = new ByteArrayInputStream(cImage);
-            BufferedImage bImageFromConvert = ImageIO.read(inputStream);
+            System.out.println("converted total images : " + convertedImages);
+            int imageNumber = 0;
+            for (byte[] cImage : convertedImages) {
+                InputStream inputStream = new ByteArrayInputStream(cImage);
+                BufferedImage bImageFromConvert = ImageIO.read(inputStream);
 
-            String imageFileName = DEST_DIR + "sample" + "-" + imageNumber + "." + OUT_IMAGE_FORMAT;
-            File outFile = new File(imageFileName);
-            ImageIO.write(bImageFromConvert, OUT_IMAGE_FORMAT, outFile);
-            System.out.println("Wrote image : " + outFile.getAbsolutePath());
-            imageNumber++;
+                String imageFileName = DEST_DIR + "sample" + "-" + imageNumber + "." + OUT_IMAGE_FORMAT;
+                File outFile = new File(imageFileName);
+                ImageIO.write(bImageFromConvert, OUT_IMAGE_FORMAT, outFile);
+                System.out.println("Wrote image : " + outFile.getAbsolutePath());
+                imageNumber++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
     }
 }
